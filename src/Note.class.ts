f9ea607afdce1184ffc1159a8f5a4dbe36dba128
Eric Dürr <eric.durr.20@ull.edu.com>
2022-04-20 21:49:06 +0100

    note: add note class, tests passed

---
+++ src/Note.class.ts
@@ -0,0 +1,44 @@
+type Color =  'yellow' | 'blue' | 'red' | 'green';
+
+export default class Note {
+  private _title: string;
+
+  private _body: string;
+
+  private _color: Color;
+
+  constructor(
+    title: string = 'New note',
+    body: string = '',
+    color: Color = 'yellow',
+  ) {
+    this._title = title;
+    this._body = body;
+    this._color = color;
+  }
+
+  get title(): string { return this._title; }
+
+  set title(value: string) { this._title = value; }
+
+  get body(): string { return this._body; }
+
+  set body(value: string) { this._body = value; }
+
+  get color(): Color { return this._color; }
+
+  public colorIsYellow() { this._color = 'yellow'; }
+
+  public colorIsBlue() { this._color = 'blue'; }
+
+  public colorIsRed() { this._color = 'red'; }
+
+  public colorIsGreen() { this._color = 'green'; }
+
+  public toString() {
+    let output = `${this.title}\n`;
+    output += `${'─'.repeat(this.title.length)}\n`;
+    output += `${this.body}\n`;
+    return output;
+  }
+}
---
+++ test/Note.test.ts
@@ -0,0 +1,49 @@
+// import Sinon from 'sinon';
+
+import { expect } from 'chai';
+import { describe, it } from 'mocha';
+import Note from '../src/Note.class';
+
+// Sinon.stub(console, 'log');
+
+describe('Default note object tests', () => {
+  const defaultNote = new Note();
+  it('Default note title is \'New note\'', () => {
+    expect(defaultNote.title).to.be.eq('New note');
+  });
+  it('Default note body is empty', () => {
+    expect(defaultNote.body).to.be.eq('');
+  });
+  it('Default note color is yellow', () => {
+    expect(defaultNote.color).to.be.eq('yellow');
+  });
+  it('Default note title can be modified', () => {
+    defaultNote.title = 'My default note';
+    expect(defaultNote.title).to.be.eq('My default note');
+  });
+  it('Default note body can be modified', () => {
+    defaultNote.body = 'Hello world';
+    expect(defaultNote.body).to.be.eq('Hello world');
+  });
+  it('Default note color can be changed to red', () => {
+    defaultNote.colorIsRed();
+    expect(defaultNote.color).to.be.eq('red');
+  });
+  it('Default note color can be changed to blue', () => {
+    defaultNote.colorIsBlue();
+    expect(defaultNote.color).to.be.eq('blue');
+  });
+  it('Default note color can be changed to green', () => {
+    defaultNote.colorIsGreen();
+    expect(defaultNote.color).to.be.eq('green');
+  });
+  it('Default note color can be changed back to yellow', () => {
+    defaultNote.colorIsYellow();
+    expect(defaultNote.color).to.be.eq('yellow');
+  });
+  it('note to string reveals title and body formatted', () => {
+    expect(defaultNote.toString()).to.be.eq('My default note\n'
+      + '───────────────\n'
+      + 'Hello world\n');
+  });
+});
